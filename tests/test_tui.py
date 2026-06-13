4b5096cfce2d5d9cae9efe9c3bb6bc6cbd1f3744
Nikolay514 <kurenkovnikolay@yandex.ru>
2026-06-13 14:59:07 +0400

    Update test_tui.py

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -1,8 +1,8 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from io import StringIO
 
-from src.db.backend.memory import StudentDB, StudentDBJSON, StudentDBCSV
+from src.db.backend.memory import StudentDB
 from src.db.tui import Tui
 
 
